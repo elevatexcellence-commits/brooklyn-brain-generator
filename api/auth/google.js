fcd22f9d0d46ce2dc537f3080649045aa8e2ad21
elevatexcellence-commits <elevate.xcellence@brooklynfitboxing.com>
2026-06-03 14:15:24 +0200

    Create google.js

---
+++ api/auth/google.js
@@ -0,0 +1,16 @@
+import { google } from 'googleapis';
+
+const oauth2Client = new google.auth.OAuth2(
+  process.env.GOOGLE_CLIENT_ID,
+  process.env.GOOGLE_CLIENT_SECRET,
+  `https://${process.env.VERCEL_URL}/api/auth/google-callback`
+);
+
+export default function handler(req, res) {
+  const authUrl = oauth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: ['https://www.googleapis.com/auth/spreadsheets.readonly']
+  });
+  
+  res.redirect(authUrl);
+}
